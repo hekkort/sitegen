522aa4f6eeb892230b45212fb238e95a70c78bc0
hekkort <harryvanderknaap@gmail.com>
2025-02-25 16:11:51 +0100

    updated splitter

--- src/split.py
+++ src/split.py
@@ -5,19 +5,19 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     node_list = []
 
     for o in old_nodes:
-        lst = o.text.split(delimiter)
-        for index, item in enumerate(lst):
-            if index % 2 == 0:
-                node_list.append(TextNode(item, o.text_type))
-            else:
-                node_list.append(TextNode(item, text_type))
+        if o.text_type == TextType.TEXT:
+            lst = o.text.split(delimiter)
+            for index, item in enumerate(lst):
+                if item.strip():
+                    if index % 2 == 0:
+                        node_list.append(TextNode(item, o.text_type))
+                    else:
+                            node_list.append(TextNode(item, text_type))
+        else:
+            node_list.append(o)
 
     return node_list
     
-
-
-
-node = TextNode("This is text with a `code block` word", TextType.NORMAL)
-new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-print(new_nodes)
+def extract_markdown_images(text):
+    pass
 
