348d4681bb6ca2a48f648187c5255be017b10a00
hekkort <harryvanderknaap@gmail.com>
2025-03-01 10:37:19 +0100

    added text_to_textnode function

--- src/split.py
+++ src/split.py
@@ -6,7 +6,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     node_list = []
 
     for o in old_nodes:
-        if o.text_type == TextType.TEXT:
+        if o.text_type == TextType.NORMAL:
             lst = o.text.split(delimiter)
             for index, item in enumerate(lst):
                 if item.strip():
@@ -27,51 +27,76 @@ def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
-def split_nodes_image(node):
-    text_list = re.split(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
-    images_dict = {}
+def split_nodes_image(old_nodes):
+    result = []
     
-    for image_text, url in extract_markdown_images(node.text):
-        images_dict[image_text] = url
+    for node in old_nodes:
+        # Only process NORMAL nodes - skip nodes of other types
+        if node.text_type != TextType.NORMAL:
+            result.append(node)
+            continue
+            
+        text_list = re.split(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
+        images_dict = {}
+        
+        for image_text, url in extract_markdown_images(node.text):
+            images_dict[image_text] = url
+        
+        # Create a set of URLs to check against
+        urls = set(images_dict.values())
+        
+        text_list = list(filter(None, text_list))
+        for element in text_list:
+            if element in images_dict:
+                result.append(TextNode(element, TextType.IMAGES, images_dict[element]))
+            elif element not in urls:
+                result.append(TextNode(element, TextType.NORMAL))
     
-    # Create a set of URLs to check against
-    urls = set(images_dict.values())
-    
-    text_list = list(filter(None, text_list))
-    final_list = []
-    for element in text_list:
-        if element in images_dict:
-            final_list.append(TextNode(element, TextType.IMAGES, images_dict[element]))
-        elif element not in urls:
-            final_list.append(TextNode(element, TextType.NORMAL))
+    return result
 
-    return final_list
-
-def split_nodes_link(node):
-    text_list = re.split(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
-    links_dict = {}
+def split_nodes_link(old_nodes):
+    result = []
     
-    for link_text, url in extract_markdown_links(node.text):
-        links_dict[link_text] = url
+    for node in old_nodes:
+        # Only process NORMAL nodes - skip nodes of other types
+        if node.text_type != TextType.NORMAL:
+            result.append(node)
+            continue
+            
+        text_list = re.split(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
+        links_dict = {}
+        
+        for link_text, url in extract_markdown_links(node.text):
+            links_dict[link_text] = url
+        
+        # Create a set of URLs to check against
+        urls = set(links_dict.values())
+        
+        text_list = list(filter(None, text_list))
+        for element in text_list:
+            if element in links_dict:
+                result.append(TextNode(element, TextType.LINKS, links_dict[element]))
+            elif element not in urls:
+                result.append(TextNode(element, TextType.NORMAL))
     
-    # Create a set of URLs to check against
-    urls = set(links_dict.values())
-    
-    text_list = list(filter(None, text_list))
-    final_list = []
-    for element in text_list:
-        if element in links_dict:
-            final_list.append(TextNode(element, TextType.LINKS, links_dict[element]))
-        elif element not in urls:
-            final_list.append(TextNode(element, TextType.NORMAL))
-    return final_list
+    return result
 
 def text_to_textnodes(text):
-    node = TextNode(text, TextType.NORMAL)
-    return split_nodes_link(node)
+    
+    nodes = [TextNode(text, TextType.NORMAL)]
 
-text = TextNode("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)", 
-                TextType.NORMAL)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    return nodes
+
+
+
+moretext = [TextNode("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)", 
+                TextType.NORMAL)]
 
 image = TextNode(
         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
@@ -83,6 +108,6 @@ website = TextNode(
     TextType.NORMAL,
 )
 
+text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
 
-print(split_nodes_link(text))
-print(split_nodes_image(text))
+print(text_to_textnodes(text))
